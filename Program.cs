1eff150e7089f5753f61e0dd54c9ed4aa6987902
Alberto-dH4 <47018430+Alberto-dH4@users.noreply.github.com>
2019-01-24 22:38:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion_Triangulo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese las variables c,z");
+            double cGrados = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+            //Conversión de ángulos a radianes
+            double c = cGrados * (Math.PI / 180.0);
+
+            //Cálculo de valores
+            double y = z / Math.Tan(c);
+            double t = z / Math.Sin(c);
+            double a = Math.Atan(y / z);
+
+            //Conversión
+            double aGrados = a / (Math.PI / 180.0);
+
+            //Imprimir salida
+            Console.WriteLine("y:" + y);
+            Console.WriteLine("t:" + t);
+            Console.WriteLine("a:" + aGrados);
+
+        }
+    }
+}
