1779decffd61260d1c2d1b0b8298bc112dc67f38
Alberto-dH4 <47018430+Alberto-dH4@users.noreply.github.com>
2019-01-29 19:38:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Ingreso de datos
+        Console.WriteLine("Ingrese las variables d°,b°,y");
+        double dGrados = double.Parse(Console.ReadLine());
+        double bGrados = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        //Conversión de ángulos a radianes
+        double d = dGrados * (Math.PI / 180.0);
+        double b = bGrados * (Math.PI / 180.0);
+
+        //Cálculo de valores
+        double c = (d + b);
+        double z = Math.Tan(c) * y;
+
+        //Imprimir salida
+        Console.WriteLine("z:" + z);
+    }
+}
