c52b455c342bec4a0cb3bc3ec494a4f1d3de2ba7
Alberto-dH4 <47018430+Alberto-dH4@users.noreply.github.com>
2019-01-31 11:38:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //$ 828,116
+        //Ingreso de datos
+        Console.WriteLine("Por favor ingrese su salario mensual.");
+        double s = double.Parse(Console.ReadLine());
+        double smmlv = 828116;
+
+        //Condicionales
+        if (s < (smmlv * 2))
+        {
+            Console.WriteLine("Le corresponde la tarifa A");
+        }
+        else if ((smmlv * 2) <= s && s < (smmlv * 4))
+        {
+            Console.WriteLine("Le corresponde la tarifa B");
+        }
+        else if (s >= (smmlv * 4))
+        {
+            Console.WriteLine("Le corresponde la tarifa c");
+        }
+    }
+}
